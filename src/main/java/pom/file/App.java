a685b44f935bf1a56ff102d682640b0067d56cee
aditya rahatekar <adityatech04@gmail.com>
2016-10-04 14:31:26 +0530

    new repo fiile for jenkins test

---
+++ src/main/java/pom/file/App.java
@@ -0,0 +1,13 @@
+package pom.file;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
